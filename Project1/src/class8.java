e58517998ba7e3e569b1ed254469262acf33f5a2
Wesley Gibbson <Wesley Gibbson@Wesly>
2019-08-05 04:37:44 +0530

    Pushing my first code

---
+++ Project1/src/class8.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+
+public class class8
+
+{
+public static void main(String[]args)
+{
+
+System.out.println("Please enter value of a ");
+
+
+
+Scanner s=new Scanner(System.in);
+
+int a= s.nextInt();
+System.out.println("value of a is "+a);
+}
+}
